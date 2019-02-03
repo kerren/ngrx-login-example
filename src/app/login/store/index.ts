ff3a06c6b88e88823c68a7aba84b7da77a4cc07e
Kerren <kerren.ortlepp@gmail.com>
2019-02-03 12:51:22 +0200

    Added the index file

---
+++ src/app/login/store/index.ts
@@ -0,0 +1,10 @@
+import * as Actions from './login.actions';
+import * as Reducers from './login.reducers';
+import * as State from './login.state';
+
+
+export {
+  Actions,
+  Reducers,
+  State
+};
