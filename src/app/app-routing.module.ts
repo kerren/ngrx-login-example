81926f1d81e5de12fd2b42b60efdcd70f4a18f65
Kerren Ortlepp <kerren@entrostat.com>
2019-02-03 17:56:51 +0200

    Revert "Feature/login module"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from "./app.component";
-import {LoginComponent} from "./login/login/login.component";
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: '',
-    component: AppComponent,
-    children: [
-      {
-        path: 'login',
-        component: LoginComponent
-      }
-    ]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/store/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './login.state';
-export * from './login.actions';
-export * from './login.reducers';
-export * from './login.selectors';
